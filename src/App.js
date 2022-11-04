27a8910aae19203f9d2e268eb3f6c7e703cf0f3b
boruch spitzer <coderoney@gmail.com>
2022-11-04 08:16:02 -0400

    1. initial setings

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>"just getting started"</h1>
+      <h1>"baby stort coming up..."</h1>
+      <img src="src\pexels-pixabay-39369.jpg" alt="picture here" />
+    </div>
+  );
+}
+
+export default App;
